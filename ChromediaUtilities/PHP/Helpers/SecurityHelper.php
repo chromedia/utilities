7cf490cfe9421d2dc07fea51e8bf6f3d16fac72a
Chris Velarde <chris.velarde@chromedia.com>
2012-07-10 09:37:01 +0800

    test for autoload

--- ChromediaUtilities/PHP/Helpers/SecurityHelper.php
+++ ChromediaUtilities/PHP/Helpers/SecurityHelper.php
@@ -1,6 +1,6 @@
 <?php
 namespace ChromediaUtilities\PHP\Helpers;
-
+//require_once realpath(dirname(__FILE__).'/../../../../../../vendor/autoload.php');
 class SecurityHelper
 {
     public static function hash_sha256($data)

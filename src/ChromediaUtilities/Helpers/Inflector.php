f0a582807870b835c7a088f98773316477db6f71
Chris Velarde <chris.velarde@chromedia.com>
2012-07-12 13:37:16 +0800

    updated inflector

--- src/ChromediaUtilities/Helpers/Inflector.php
+++ src/ChromediaUtilities/Helpers/Inflector.php
@@ -5,6 +5,12 @@ class Inflector
 {
     static public function camelize($word)
     {
-        return str_replace(' ','',preg_replace('/[^A-Z^a-z^0-9]+/',' ',$word));
+        return str_replace(' ','',ucwords(preg_replace('/[^A-Z^a-z^0-9]+/',' ',$word)));
+    }
+    
+    static public function toVariable($word)
+    {
+        $word = Inflector::camelize($word);
+        return strtolower($word[0]).substr($word,1);
     }
 }
